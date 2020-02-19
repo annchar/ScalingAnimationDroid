def6f2eceac29ec412fa47005559c38a99617a9c
Chanoknad Mekaew <ann.skr14@gmail.com>
2020-02-19 15:06:27 +0700

    Rename 'MainActivity' to 'DemoActivity'

--- app/src/main/java/com/annchar/scalinganimationdroid/DemoActivity.kt
+++ app/src/main/java/com/annchar/scalinganimationdroid/DemoActivity.kt
@@ -3,10 +3,10 @@ package com.annchar.scalinganimationdroid
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class DemoActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_demo)
     }
 }
